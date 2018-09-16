2d6389267aeec5728dc320327c2b519af3dec1d8
Jason Tu <jasontu4@gmail.com>
2018-09-16 15:08:36 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+(From [Leftover Salad](http://leftoversalad.com/c/015_programmingpeople/).)
+
+<img src="https://user-images.githubusercontent.com/914228/45598510-e816b980-b991-11e8-81e3-bccae0db4718.png" alt="Elixir sorcerer" width="300px">
---
+++ lib/scenic_planet.ex
@@ -0,0 +1,20 @@
+defmodule ScenicPlanet do
+  @moduledoc """
+  Starter application using the Scenic framework.
+  """
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    # load the viewport configuration from config
+    main_viewport_config = Application.get_env(:scenic_planet, :viewport)
+
+    # start the application with the viewport
+    children = [
+      supervisor(ScenicPlanet.Sensor.Supervisor, []),
+      supervisor(Scenic, [viewports: [main_viewport_config]]),
+    ]
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
+
+end
