67e8d306d62275f3abd1e94ef3b271e3d7c25dc5
Jason Tu <jasontu4@gmail.com>
2018-09-16 15:50:58 -0700

    Add player callbacks, and plan out math utils

--- lib/scenes/prototype.ex
+++ lib/scenes/prototype.ex
@@ -5,18 +5,74 @@ defmodule ScenicPlanet.Scene.Prototype do
 
   @player_width 10
   @player_height 25
+  @update_every_ms 16
 
   @graph Graph.build()
          |> rect({@player_width, @player_height}, fill: :light_coral)
 
-  def init(nil, opts) do
-    IO.puts("hello world, it's working!")
+  #
+  # Init callback.
+  #
 
+  def init(nil, _opts) do
+    # Define our state.
     state = %{
-      graph: @graph
+      graph: @graph,
+
+      # Player properties.
+      pos: {0, 0},
+      seek_to: {200, 200}
     }
 
+    # Set up our game loop.
+    :timer.send_interval(@update_every_ms, :update)
+
+    # Push our graph down to the viewport.
     push_graph(@graph)
+
+    # Return OK tuple.
     {:ok, state}
   end
+
+  #
+  # Math utils.
+  #
+
+  #
+  # Player entity: updating player state.
+  #
+
+  # State -> State
+  def player_update(%{pos: pos, seek_to: seek_to} = state) do
+    # Move towards `seek_to`.
+  end
+
+  #
+  # Player entity: updating graph.
+  #
+
+  # Graph -> State -> Graph
+  def player_draw() do
+  end
+
+  #
+  # Update callback.
+  #
+
+  def handle_info(:update, state) do
+    # IO.puts("update")
+    {:noreply, state}
+  end
+
+  #
+  # Handle input.
+  #
+
+  # Update `seek_to` variable.
+  def handle_input({:cursor_button, {:left, :press, _, {_x, _y} = seek_to}}, _context, state) do
+    state = %{state | seek_to: seek_to}
+    {:noreply, state}
+  end
+
+  def handle_input(_input, _context, state), do: {:noreply, state}
 end
