2c2e0bcc4eb9efc580d2680f629dc3e36851d6f8
Jason Tu <jasontu4@gmail.com>
2018-09-16 16:19:24 -0700

    Implement "click to move"

--- lib/scenes/prototype.ex
+++ lib/scenes/prototype.ex
@@ -1,6 +1,6 @@
 defmodule ScenicPlanet.Scene.Prototype do
   use Scenic.Scene
-  import Scenic.Primitives, only: [{:rect, 3}]
+  import Scenic.Primitives, only: [{:rect, 3}, {:update_opts, 2}]
   alias Scenic.Graph
 
   @player_width 10
@@ -8,7 +8,7 @@ defmodule ScenicPlanet.Scene.Prototype do
   @update_every_ms 16
 
   @graph Graph.build()
-         |> rect({@player_width, @player_height}, fill: :light_coral)
+         |> rect({@player_width, @player_height}, id: :player, fill: :light_coral)
 
   #
   # Init callback.
@@ -38,6 +38,28 @@ defmodule ScenicPlanet.Scene.Prototype do
   # Math utils.
   #
 
+  def vec3_sub({ax, ay}, {bx, by}) do
+    {ax - bx, ay - by}
+  end
+
+  def vec3_add({ax, ay}, {bx, by}) do
+    {ax + bx, ay + by}
+  end
+
+  # TODO: Handle zero vectors, to avoid division by zero.
+  def vec3_normalize({x, y} = v) do
+    m = vec3_magnitude(v)
+    {x / m, y / m}
+  end
+
+  def vec3_magnitude({x, y}) do
+    :math.sqrt(x * x + y * y)
+  end
+
+  def vec3_scale({x, y}, c) do
+    {x * c, y * c}
+  end
+
   #
   # Player entity: updating player state.
   #
@@ -45,6 +67,9 @@ defmodule ScenicPlanet.Scene.Prototype do
   # State -> State
   def player_update(%{pos: pos, seek_to: seek_to} = state) do
     # Move towards `seek_to`.
+    diff = vec3_scale(vec3_normalize(vec3_sub(seek_to, pos)), 5)
+    new_pos = vec3_add(pos, diff)
+    %{state | pos: new_pos}
   end
 
   #
@@ -52,16 +77,25 @@ defmodule ScenicPlanet.Scene.Prototype do
   #
 
   # Graph -> State -> Graph
-  def player_draw() do
+  def player_draw(graph, %{pos: pos} = _state) do
+    Graph.modify(graph, :player, &update_opts(&1, translate: pos))
   end
 
   #
   # Update callback.
   #
 
-  def handle_info(:update, state) do
-    # IO.puts("update")
-    {:noreply, state}
+  def handle_info(:update, %{graph: graph} = state) do
+    # Update game state.
+    state = player_update(state)
+
+    # Draw game state, by pushing state to viewport.
+    graph
+    |> player_draw(state)
+    |> push_graph()
+
+    # Return updated state.
+    {:noreply, %{state | graph: graph}}
   end
 
   #
